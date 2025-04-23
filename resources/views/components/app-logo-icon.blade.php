bba40df7d60b10df818fb49574b47194f27563dc
latiful <latifulamshar@gmail.com>
2025-04-23 22:04:02 +0700

    Mengubah Logo Company haha suiii

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,8 +1,27 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 42" {{ $attributes }}>
-    <path 
-        fill="currentColor" 
-        fill-rule="evenodd" 
-        clip-rule="evenodd"
-        d="M17.2 5.633 8.6.855 0 5.633v26.51l16.2 9 16.2-9v-8.442l7.6-4.223V9.856l-8.6-4.777-8.6 4.777V18.3l-5.6 3.111V5.633ZM38 18.301l-5.6 3.11v-6.157l5.6-3.11V18.3Zm-1.06-7.856-5.54 3.078-5.54-3.079 5.54-3.078 5.54 3.079ZM24.8 18.3v-6.157l5.6 3.111v6.158L24.8 18.3Zm-1 1.732 5.54 3.078-13.14 7.302-5.54-3.078 13.14-7.3v-.002Zm-16.2 7.89 7.6 4.222V38.3L2 30.966V7.92l5.6 3.111v16.892ZM8.6 9.3 3.06 6.222 8.6 3.143l5.54 3.08L8.6 9.3Zm21.8 15.51-13.2 7.334V38.3l13.2-7.334v-6.156ZM9.6 11.034l5.6-3.11v14.6l-5.6 3.11v-14.6Z"
-    />
-</svg>
+<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <style>
+      .angka-satu {
+        font-size: 80px;
+        font-weight: bold;
+        fill: currentColor; /* Menggunakan warna teks saat ini */
+      }
+      .grafik-batang {
+        fill: currentColor; /* Menggunakan warna teks saat ini */
+        opacity: 0.7; /* Sedikit transparan agar terlihat berbeda dari angka */
+      }
+      .garis-grafik {
+        fill: none;
+        stroke: currentColor; /* Menggunakan warna teks saat ini */
+        stroke-width: 2;
+      }
+    </style>
+  
+    <text x="10" y="80" class="angka-satu">1</text>
+  
+    <rect x="35" y="60" width="8" height="20" class="grafik-batang" />
+    <rect x="45" y="45" width="8" height="35" class="grafik-batang" />
+    <rect x="55" y="55" width="8" height="25" class="grafik-batang" />
+    <rect x="65" y="30" width="8" height="50" class="grafik-batang" />
+  
+    <polyline points="39,80 49,65 59,75 69,50" class="garis-grafik" />
+  </svg>
