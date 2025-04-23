b3c492a8f3548af348948ffba5e7c0994aaf11fb
latiful <latifulamshar@gmail.com>
2025-04-23 17:23:05 +0700

    Menambahkan pesan informasi di halaman dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,7 +16,6 @@
         {{-- Area untuk Tabel atau Chart --}}
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
     </div>
 </x-layouts.app>
